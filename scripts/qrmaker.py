405e237537029e4e904a0544376267adfc8b261e
azatsololearn <azat.melikyan@sololearn.com>
2023-08-25 15:46:55 +0400

    refined colors

--- scripts/qrmaker.py
+++ scripts/qrmaker.py
@@ -1,5 +1,5 @@
 import pyqrcode
-url = pyqrcode.create('2pet.pet')
+url = pyqrcode.create('berlinthepoodle.com')
 url.svg('uca-url.svg', scale=8)
 url.eps('uca-url.eps', scale=2)
 print(url.terminal(quiet_zone=1))
